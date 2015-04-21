c8c17f6966a9dc949e8309ed6b3f1cc8003364b4
Pete Bachant <petebachant@gmail.com>
2015-04-20 22:04:51 -0400

    Add K transport bar graph figure

--- getfigs.py
+++ getfigs.py
@@ -15,7 +15,8 @@ figlist = ["cp_curves.eps",
            "cd_curves.eps",
            "re_dep_cp.eps",
 	   "meancontquiv.eps",
-	   "k_contours.eps"]
+	   "k_contours.eps",
+	   "K_trans_bar_graph.eps"]
 
 for fig in figlist:
     shutil.copy2(os.path.join(figdir, fig), os.path.join("figures", fig))
