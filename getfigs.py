48cdd359b3413d2c77fcf2aba98667cf68ff6c47
Pete Bachant <petebachant@gmail.com>
2016-01-16 13:03:16 -0500

    Copy figures from local submodule repo

--- getfigs.py
+++ getfigs.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
-"""
-This script copies figures from the RM2 tow tank
-experiment directory.
-"""
+"""This script copies figures from the RM2 tow tank experiment directory."""
 
 import os
 import sys
 import shutil
 
-figdir = os.path.join(os.path.expanduser("~"), "Research",
-        "Experiments", "RM2 tow tank", "Figures")
+figdir = os.path.join("RM2-tow-tank", "Figures")
 
 figlist = ["cp_curves.eps",
            "cd_curves.eps",
            "perf_re_dep.eps",
-	   "meancontquiv.eps",
-	   "k_contours.eps",
-	   "K_trans_bar_graph.eps",
-	   "no_blades_all.eps",
-	   "perf_covers.eps"]
+    	   "meancontquiv.eps",
+           "k_contours.eps",
+    	   "K_trans_bar_graph.eps",
+    	   "no_blades_all.eps",
+    	   "perf_covers.eps"]
 
 for fig in figlist:
     shutil.copy2(os.path.join(figdir, fig), os.path.join("figures", fig))
