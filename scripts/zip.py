62b6bb2f8f05a749d687a5491e14b920f68c2220
Pete Bachant <petebachant@gmail.com>
2016-03-15 17:17:24 -0400

    Add zip script

---
+++ scripts/zip.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""This script zips up all the necessary source files to compile the paper."""
+
+from zipfile import ZipFile
+from subprocess import check_output
+import os
+
+# Describe version via git
+version = check_output(["git", "describe"]).strip().decode()
+
+figures = ["figures/" + f for f in os.listdir("figures") if f.endswith(".eps")]
+
+files = ["paper.tex", "PLOS-Submission.eps", "plos2015.bst"]
+
+with ZipFile("archive/paper-{}.zip".format(version), "w") as f:
+    for fig in figures:
+        f.write(fig)
+    for file in files:
+        f.write(file)
